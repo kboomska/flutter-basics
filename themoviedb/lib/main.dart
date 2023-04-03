09fe5a20db0ea022e7059f530af30f55613948c4
Aleksey Kuzmin <kboomska@gmail.com>
2023-04-03 16:02:33 +0300

    Lesson 30: Basic Layout. The Movie Database login screen

---
+++ themoviedb/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/auth/auth_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'TMDB',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromRGBO(3, 37, 65, 1),
+        ),
+      ),
+      home: AuthWidget(),
+    );
+  }
+}
---
+++ themoviedb/lib/widgets/auth/auth_widget.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/material.dart';
+
+import '../../theme/app_button_style.dart';
+
+class AuthWidget extends StatefulWidget {
+  const AuthWidget({super.key});
+
+  @override
+  State<AuthWidget> createState() => _AuthWidgetState();
+}
+
+class _AuthWidgetState extends State<AuthWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login to your account'),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          _HeaderWidget(),
+        ],
+      ),
+    );
+  }
+}
+
+class _HeaderWidget extends StatelessWidget {
+  const _HeaderWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const textStyle = TextStyle(
+      fontSize: 16,
+      color: Colors.black,
+    );
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 16,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 25,
+          ),
+          const _FormWidget(),
+          const SizedBox(
+            height: 25,
+          ),
+          const Text(
+            'In order to use the editing and rating capabilities of TMDb, as well as get personal recommendations you will need to login to your account. If you do not have an account, registering for an account is free and simple.',
+            style: textStyle,
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          TextButton(
+            onPressed: () {},
+            style: AppButtonStyle.linkButton,
+            child: const Text('Register'),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          const Text(
+            'If you signed up but didn\'t get your verification email.',
+            style: textStyle,
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          TextButton(
+            onPressed: () {},
+            style: AppButtonStyle.linkButton,
+            child: const Text('Verify email'),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _FormWidget extends StatefulWidget {
+  const _FormWidget({super.key});
+
+  @override
+  State<_FormWidget> createState() => __FormWidgetState();
+}
+
+class __FormWidgetState extends State<_FormWidget> {
+  final _loginTextController = TextEditingController();
+  final _passwordTextController = TextEditingController();
+
+  String? errorText = null;
+
+  void _auth() {
+    final login = _loginTextController.text;
+    final password = _passwordTextController.text;
+
+    if (login == 'admin' && password == 'admin') {
+      errorText = null;
+      print('open app');
+    } else {
+      errorText = 'Неверный логин или пароль';
+      print('show error');
+    }
+
+    setState(() {});
+  }
+
+  void _resetPassword() {
+    print('reset password');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const textFieldColor = Color(0xFF01B4E4);
+
+    const textStyle = TextStyle(
+      fontSize: 16,
+      color: Color(0xFF212529),
+    );
+
+    const inputDecoration = InputDecoration(
+      border: OutlineInputBorder(),
+      contentPadding: EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 10,
+      ),
+      isCollapsed: true,
+    );
+
+    final errorText = this.errorText;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (errorText != null) ...[
+          Text(
+            errorText,
+            style: const TextStyle(
+              fontSize: 17,
+              color: Colors.red,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
+        const Text(
+          'Username',
+          style: textStyle,
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        TextField(
+          controller: _loginTextController,
+          decoration: inputDecoration,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        const Text(
+          'Password',
+          style: textStyle,
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        TextField(
+          controller: _passwordTextController,
+          decoration: inputDecoration,
+          obscureText: true,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Row(
+          children: [
+            TextButton(
+              onPressed: _auth,
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(textFieldColor),
+                foregroundColor: MaterialStateProperty.all(Colors.white),
+                overlayColor: MaterialStateProperty.all(Colors.lightBlueAccent),
+                textStyle: MaterialStateProperty.all(const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                )),
+                padding: MaterialStateProperty.all(
+                    const EdgeInsets.symmetric(horizontal: 15)),
+              ),
+              child: const Text('Login'),
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            TextButton(
+              onPressed: _resetPassword,
+              style: AppButtonStyle.linkButton,
+              child: const Text('Reset password'),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
